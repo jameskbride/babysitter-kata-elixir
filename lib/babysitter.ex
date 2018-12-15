c3e58350ce01d13bd4939b62f1b5acc7bab5c23d
Jim Kirkbride <james.kirkbride@gmail.com>
2018-12-15 16:42:07 -0500

    Refactoring.

--- lib/babysitter.ex
+++ lib/babysitter.ex
@@ -1,9 +1,12 @@
 defmodule BabySitter do
 
+  @earliest_start_time 17
+  @early_hourly_rate 12
+
   def calculate_pay(start_time, hours, _bed_time) do
     cond do
-      start_time < 17 -> {:error, "Babysitter cannot start before 5:00PM"}
-      true -> 12 * hours
+      start_time < @earliest_start_time -> {:error, "Babysitter cannot start before 5:00PM"}
+      true -> @early_hourly_rate * hours
     end
   end
 end
